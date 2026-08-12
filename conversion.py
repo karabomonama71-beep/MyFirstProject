8e0df0394d3b6b70528ffc7526d29903e1f468e0
KaraboMonama <292044079+karabomonama71-beep@users.noreply.github.com>
2026-08-12 10:51:01 +0200

    Add HyperionDev Python tasks

---
+++ conversion.py
@@ -0,0 +1,19 @@
+# Pseudocode:
+# Create num1 with the value 99.23
+# Create num2 with the value 23
+# Create num3 with the value 150
+# Create string1 with the value "100"
+# Convert num1 to an integer
+# Convert num2 to a float
+# Convert num3 to a string
+# Convert string1 to an integer
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+num1 = int(num1)
+num2 = float(num2)
+num3 = str(num3)
+string1 = int(string1)
---
+++ hellow_world.py
@@ -0,0 +1,14 @@
+# Pseudocode:
+# Ask the user for their name
+# Print the user's name
+# Ask the user for their age
+# Print the user's age
+# Print "Hello World!"
+
+name = input("Enter your name: ")
+print(name)
+
+age = input("Enter your age: ")
+print(age)
+
+print("Hello World!")
